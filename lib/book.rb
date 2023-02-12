5bb6545936fe7e3d96318e0bae82e7c45fdee1d7
David <david.owuor@student.moringaschool.com>
2023-02-12 15:35:51 +0300

    complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
 
 
+class Book
+    attr_accessor  :page_count, :genre, :title, :author
+def initialize(title = 'Putting')
+    @title = title
+end
+
+def turn_page
+   puts "Flipping the page...wow, you read fast!"
+end
+end
+
+book = Book.new('New')
+
